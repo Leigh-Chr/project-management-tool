3623165b2809d8b8b89fbf1af8e949f110b87103
Virgil CHAREYRE <virgil.07@hotmail.fr>
2024-09-11 22:09:39 +0200

    feat: add HomeComponent with welcome message and navigation buttons
    - Created `HomeComponent` in `src/app/features/home`.
    - Integrated `ButtonComponent` for reusable button links.
    - Designed responsive and dark-mode friendly UI using Tailwind CSS.
    - Added buttons for "Register" and "Login" navigation.

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../ui/button.component';
+
+@Component({
+  imports: [ButtonComponent],
+  standalone: true,
+  template: `
+    <div
+      class="shadow-md p-8 rounded-lg w-full max-w-md bg-white dark:bg-neutral-900"
+    >
+      <h1 class="mb-6 font-bold text-4xl">
+        Welcome to Project Management Tool
+      </h1>
+      <p
+        class="mb-8 text-center text-lg text-neutral-600 dark:text-neutral-400"
+      >
+        Plan, track, and collaborate on projects efficiently.
+      </p>
+      <div class="flex space-x-4">
+        <ui-button [isLink]="true" href="/register" label="Register" />
+        <ui-button [isLink]="true" href="/login" label="Login" />
+      </div>
+    </div>
+  `,
+  host: { class: 'grid h-screen place-items-center' },
+})
+export class HomeComponent {}
