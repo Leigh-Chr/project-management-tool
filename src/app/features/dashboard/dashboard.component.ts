cdb55fb8a058d67106e0dc4930c6a00c68e81915
Virgil CHAREYRE <virgil.07@hotmail.fr>
2024-09-11 22:32:49 +0200

    feat: add DashboardComponent with panels
    - Created `DashboardComponent` to serve as the main dashboard view.
    - Utilized `DefaultLayoutComponent` for consistent application layout.
    - Imported and integrated `ProjectsPanelComponent`, `TasksPanelComponent`, and `TaskHistoriesPanelComponent` to display respective data.
    - Arranged components in a responsive grid layout using Tailwind CSS.
    - Nested project and task panels in a larger column and task histories in a smaller column for a balanced dashboard view.

---
+++ src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { DefaultLayoutComponent } from '../../layouts/default-layout.component';
+import { PaginatorComponent } from '../../ui/paginator.component';
+import { ProjectsPanelComponent } from './panels/projects-panel.component';
+import { TaskHistoriesPanelComponent } from './panels/task-histories-panel.component';
+import { TasksPanelComponent } from './panels/tasks-panel.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    DefaultLayoutComponent,
+    PaginatorComponent,
+    ProjectsPanelComponent,
+    TasksPanelComponent,
+    TaskHistoriesPanelComponent,
+  ],
+  template: `
+    <default-layout>
+      <div class="gap-4 grid lg:grid-cols-[2fr,1fr] p-4">
+        <div class="gap-4 grid lg:grid-cols-1">
+          <projects-panel />
+          <tasks-panel />
+        </div>
+        <task-histories-panel />
+      </div>
+    </default-layout>
+  `,
+})
+export class DashboardComponent {}
