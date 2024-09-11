3454c46f6f4b526823244c611c2c229d5c10dcf0
Virgil CHAREYRE <virgil.07@hotmail.fr>
2024-09-11 19:57:51 +0200

    feat: add route components and animations provider
    - Added Angular animations provider in app.config.ts.
    - Imported and added `DashboardComponent`, `HomeComponent`, `RegisterComponent`, `LoginComponent`, and `ProjectsComponent` to routes in app.routes.ts.
    - Defined path routes for the imported components.
    - Set up a default route to redirect to '/home'.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { HomeComponent } from './features/home/home.component';
+import { RegisterComponent } from './features/register/register.component';
+import { LoginComponent } from './features/login/login.component';
+import { ProjectsComponent } from './features/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
