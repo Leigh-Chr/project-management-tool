7b4d4b59d24ebe7846a6497a0bad7ec8912d1949
Virgil CHAREYRE <virgil.07@hotmail.fr>
2024-09-25 16:42:27 +0200

    Enhance Tasks Panel with project association and additional task details
    - Updated `TasksPanelComponent` to include project names for each task by integrating `ProjectService`.
    - Added project and priority columns to the task table for better task overview.
    - Updated task data mapping to associate each task with its respective project name.
    - Refactored imports in `tasks-panel.component.ts` for clarity and organization.
    - Sorted tasks by due date and filtered them by the current user's assignments.
    - Included priority field in task listing for improved task management.

--- src/app/features/dashboard/panels/tasks-panel.component.ts
+++ src/app/features/dashboard/panels/tasks-panel.component.ts
@@ -5,12 +5,13 @@ import {
   computed,
   inject,
 } from '@angular/core';
-import { Task } from '../../../core/services/data-mock.service';
+import { AuthService } from '../../../core/services/auth.service';
+import { Project, Task } from '../../../core/services/data-mock.service';
+import { ProjectService } from '../../../core/services/project.service';
 import { TaskService } from '../../../core/services/task.service';
+import { Table } from '../../../types';
 import { ButtonComponent } from '../../../ui/button.component';
 import { PaginatorComponent } from '../../../ui/paginator.component';
-import { Table } from '../../../types';
-import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'tasks-panel',
@@ -77,29 +78,48 @@ import { AuthService } from '../../../core/services/auth.service';
 export class TasksPanelComponent {
   private readonly authService = inject(AuthService);
   readonly taskService = inject(TaskService);
-  readonly table: Table<Task> = {
+  private readonly projectService = inject(ProjectService);
+
+  readonly table: Table<
+    Task & {
+      project: Project['name'];
+    }
+  > = {
     headers: [
       { name: 'ID', key: 'id' },
       { name: 'Name', key: 'name' },
+      { name: 'Project', key: 'project' },
       { name: 'Description', key: 'description' },
       { name: 'Due Date', key: 'dueDate' },
+      { name: 'Priority', key: 'priority' },
     ],
     items: [
       { key: 'id', type: 'number' },
       { key: 'name', type: 'text' },
+      { key: 'project', type: 'text' },
       { key: 'description', type: 'text' },
       { key: 'dueDate', type: 'date' },
+      { key: 'priority', type: 'number' },
     ],
   };
 
-  readonly tasks = computed(() => {
+  readonly tasks = computed<(Task & { project: Project['name'] })[]>(() => {
     const currentUserId = this.authService.userSignal()?.id;
     if (!currentUserId) return [];
 
     return this.taskService
       .tasksSignal()
       .filter((task) => task.assigneeId === currentUserId)
-      .sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime());
+      .sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime())
+      .map((task) => {
+        return {
+          ...task,
+          project:
+            this.projectService
+              .projectsSignal()
+              .find((project) => project.id === task.projectId)?.name ?? '',
+        };
+      });
   });
 
   onPageChange(page: number) {
