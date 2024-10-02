c2babd729832b102baf3d6ae303ee5989dd6758e
Virgil CHAREYRE <virgil.07@hotmail.fr>
2024-10-02 12:40:22 +0200

    fix: update text color in select field component for dark mode
    - Added dark mode text color `dark:text-neutral-300` to ensure proper text visibility in dark theme.

--- src/app/ui/select-field.component.ts
+++ src/app/ui/select-field.component.ts
@@ -24,7 +24,7 @@ type SelectOption<T> = {
         [formControl]="control"
         [id]="id"
         class="
-        px-4 py-2 text-sm text-neutral-700 bg-neutral-50 dark:bg-neutral-900 
+        px-4 py-2 text-sm text-neutral-700 dark:text-neutral-300 bg-neutral-50 dark:bg-neutral-900 
         border border-neutral-300 dark:border-neutral-700 rounded-lg shadow-sm
         focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
         dark:focus:ring-blue-600
