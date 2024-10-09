565b8359d53d6072041fe24cd385dc622a4ebcde
Virgil CHAREYRE <virgil.07@hotmail.fr>
2024-10-09 16:37:27 +0200

    add(ui): enhance button component with icon and tooltip support
      - Integrates `IconComponent` and `TooltipDirective` into the button component for enhanced functionality.
      - Adds support for rendering icons with `ui-icon` and tooltips when `iconOnly` is true.
      - Improves accessibility by using `aria-label` and `aria-disabled` attributes.
      - Refines button styles and class management with additional flexibility for custom classes.

--- src/app/ui/button.component.ts
+++ src/app/ui/button.component.ts
@@ -1,59 +1,75 @@
 import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { IconComponent } from './icon.component';
+import { TooltipDirective } from './tooltip.directive';
 
 @Component({
   selector: 'ui-button',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, IconComponent, TooltipDirective],
   template: `
-    @if ( isLink ) {
-
-    <a [href]="href" [class.disabled]="disabled" [ngClass]="buttonClasses">
+    @if (isLink) {
+    <a
+      attr.aria-label="{{ label }}"
+      [href]="href"
+      [attr.aria-disabled]="disabled"
+      [ngClass]="buttonClasses"
+      role="button"
+    >
       @if (icon) {
-      <i class="{{ icon }}"></i>
+      <ui-icon [class]="icon"></ui-icon>
+      } @if (!iconOnly) {
+      <span>{{ label }}</span>
       }
-      <span>
-        {{ label }}
-      </span>
     </a>
     } @else {
-    <button [disabled]="disabled" [ngClass]="buttonClasses">
+    <button
+      [tooltip]="iconOnly ? label : ''"
+      attr.aria-label="{{ label }}"
+      [disabled]="disabled"
+      [ngClass]="buttonClasses"
+      [attr.aria-disabled]="disabled"
+    >
       @if (icon) {
-      <i class="{{ icon }}"></i>
+      <ui-icon [class]="icon"></ui-icon>
+      } @if (!iconOnly) {
+      <span>{{ label }}</span>
       }
-      <span>
-        {{ label }}
-      </span>
     </button>
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
-  @Input() label = '';
+  @Input({
+    required: true,
+  })
+  label!: string;
+
+  @Input() iconOnly?: boolean;
   @Input() icon?: string;
   @Input() disabled?: boolean;
-  @Input() isLink = false;
+  @Input() isLink?: boolean;
   @Input() href?: string;
-  @Input() variant: 'primary' | 'secondary' | 'success' | 'danger' = 'primary';
+  @Input() variant?: 'primary' | 'secondary' | 'danger' | 'success';
+  @Input() class?: string;
 
   get buttonClasses(): string {
-    const baseClasses =
-      'py-2 px-4 font-semibold rounded-md flex items-center justify-center gap-2 shadow-md transition-all duration-200 ease-in-out';
+    const baseClasses = `px-3 py-2 font-semibold rounded-md flex items-center justify-center gap-2 shadow-md transition-all duration-200 ease-in-out`;
     const stateClasses =
       'disabled:opacity-50 disabled:pointer-events-none enabled:hover:brightness-125';
 
     const variantClasses = {
       primary:
-        'bg-gradient-to-br from-indigo-500 to-indigo-700 text-white dark:from-indigo-700 dark:to-indigo-900 dark:text-gray-200',
+        'bg-gradient-to-br from-indigo-500 to-indigo-700 text-white dark:from-indigo-700 dark:to-indigo-900 dark:text-neutral-200',
       secondary:
-        'bg-gradient-to-br from-gray-500 to-gray-700 text-white dark:from-gray-700 dark:to-gray-900 dark:text-gray-200',
-      success:
-        'bg-gradient-to-br from-green-500 to-green-700 text-white dark:from-green-700 dark:to-green-900 dark:text-gray-200',
+        'bg-gradient-to-br from-neutral-50 to-neutral-200 text-neutral-900 dark:from-neutral-700 dark:to-neutral-900 dark:text-neutral-200',
       danger:
-        'bg-gradient-to-br from-red-500 to-red-700 text-white dark:from-red-700 dark:to-red-900 dark:text-gray-200',
-    }[this.variant];
+        'bg-gradient-to-br from-red-500 to-red-700 text-white dark:from-red-700 dark:to-red-900 dark:text-neutral-200',
+      success:
+        'bg-gradient-to-br from-green-500 to-green-700 text-white dark:from-green-700 dark:to-green-900 dark:text-neutral-200',
+    }[this.variant ?? 'primary'];
 
-    return `${baseClasses} ${stateClasses} ${variantClasses}`;
+    return `${baseClasses} ${stateClasses} ${variantClasses} ${this.class}`;
   }
 }
